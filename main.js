e25c9f4d69ae18f7e8be99b6185ebad5f99b7ee0
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-02-02 23:49:35 +0000

    added darken button

--- main.js
+++ main.js
@@ -13,19 +13,34 @@ const imgAlt = ['pic of eyes', 'picture of a rock', 'picture of purple flowers',
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-for(i = 0; i < imgArr.length ; i++){
+for(let i = 0; i < imgArr.length ; i++){ //fogot let here and it broke the eventlistener not work
 
     const newImage = document.createElement('img');
+    
     newImage.setAttribute('src', 'images/'+imgArr[i]);
+    
     newImage.setAttribute('alt', imgAlt[i]);
-    thumbBar.appendChild(newImage);
 
+    newImage.addEventListener("click", ()=>{
+        displayedImage.setAttribute('src','images/'+imgArr[i])
+        displayedImage.setAttribute('alt', imgAlt[i])
+    });
+    thumbBar.appendChild(newImage);
+    
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", darkmode);
 
 function darkmode() {
-    const dark
-
-
+    if(btn.getAttribute("class") == "dark"){
+        btn.setAttribute("class", 'light');
+        btn.textContent = "Light";
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }
+    else if(btn.getAttribute("class") == "light"){
+        btn.setAttribute("class","dark")
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(78,0,0,0)";
+    }
 }
